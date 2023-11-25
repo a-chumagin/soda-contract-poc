fc66d3a1325c3d292e47b4330a21e310235ae460
achumagin <achumagin@hotmail.com>
2023-11-25 23:28:23 +0300

    chore: added docker container for apps

--- scripts/db_operations.py
+++ scripts/db_operations.py
@@ -1,5 +1,7 @@
 import vertica_python
 import yaml
+import time
+import os
 
 def read_vertica_connection_config(config_path):
     with open(config_path, 'r') as config_file:
@@ -7,9 +9,9 @@ def read_vertica_connection_config(config_path):
         vertica_config = config_data.get('data_source vertica_local', {}).get('connection')
     return vertica_config
 
-def create_vertica_connection(config):
+def create_vertica_connection(config, max_retries=3):
     conn_info = {
-        'host': config.get('host'),
+        'host': os.environ.get('vertica_host', config.get('host')),
         'port': config.get('port'),
         'user': config.get('username'),
         'password': config.get('password'),
@@ -17,8 +19,18 @@ def create_vertica_connection(config):
         'schema': config.get('schema'),
         'ssl': False  # Set to True if using SSL
     }
-    connection = vertica_python.connect(**conn_info)
-    return connection
+
+    for i in range(max_retries):
+        print(conn_info['host'])
+        try:
+            connection = vertica_python.connect(**conn_info)
+            return connection
+        except vertica_python.errors.ConnectionError:
+            if i < max_retries - 1:
+                time.sleep(5)
+                continue
+            else:
+                raise
 
 def get_vertica_table_structure(table_name, connection):
     with connection as conn:
